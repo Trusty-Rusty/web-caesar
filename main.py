682157ac658b281c7685ad8550680d0d80ef2b4a
Andrew Russell <andy.russell.code@gmail.com>
2017-03-25 11:15:49 -0500

    Wrap commit in text area and added submit button

--- main.py
+++ main.py
@@ -21,7 +21,10 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         message = "Hello World!"
         encrypted_message = caesar.encrypt(message, 13)
-        self.response.write(encrypted_message)
+        text_area =  ("<textarea>" + encrypted_message + "</textarea>")
+        button = "<input type='submit'/>"
+        form = ("<form>" + text_area + "<br>" + button + "</form>")
+        self.response.write(form)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
